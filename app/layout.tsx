0981735e9b7462ba7e3cff2ae9a3f92ed9050d6d
Morymirco <morykoulibaly996@gmail.com>
2024-10-01 11:42:49 +0000

    'tout'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import './ui/global.css';
+import { inter } from './ui/fonts';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* En ajoutant Interà l' <body>élément, la police sera appliquée dans toute votre application. Ici, vous ajoutez également le Tailwindantialiasedclasse qui lisse la police. Il n'est pas nécessaire d'utiliser cette classe, mais elle ajoute une touche agréable. */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
